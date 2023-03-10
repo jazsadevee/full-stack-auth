7b9c4aba04053f6af1c1577fabe4a7de56656405
JazSade Villian <jazsadevee@gmail.com>
2023-03-10 13:39:46 -0500

    frontend localstorage

--- client/src/api/apiConfig.js
+++ client/src/api/apiConfig.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
+const LOCALSTORAGE_KEY = 'token'
+
+const token = localStorage.getItem(LOCALSTORAGE_KEY)
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/'
+  baseURL: 'http://localhost:8080/api/',
+  Headers: {
+    'Authorization': token
+  }
 })
 
 
