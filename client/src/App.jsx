981b234952af37940724b2ffe8d86b290e6cdc8e
JazSade Villian <jazsadevee@gmail.com>
2023-03-10 10:19:33 -0500

    text box for username and new twuut

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+import NewPost from './pages/NewPost';
 import PostDetails from './pages/PostDetails';
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
-      <Link to="/">Home</Link>
+      <p>
+        <Link to="/">Home</Link>
+      </p>
+      <p>
+        <Link to={"/new"}>New Twuut</Link>
+      </p>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:handle" element={<Profile />} />
         <Route path="/post/:id" element={<PostDetails />} />
+        <Route path="/new" element={<NewPost />} />
       </Routes>
     </>
   );
